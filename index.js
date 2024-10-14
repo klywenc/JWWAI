3b2a193372b1aa9e384a9a8a4ea2f7846d811fab
Megu <43895321+klywenc@users.noreply.github.com>
2024-10-14 11:07:50 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const { format } = require('string-kit');
+
+// To cos formatuje wiadomosci w konsoli
+const formattedMessage = format('Siema, ^r%s^! Mam dla ciebie: %d + %d = %d', 'gościu', 5, 3, 5 + 3);
+
+console.log(formattedMessage);  
